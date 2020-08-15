da53b2454b4c8c169e5db5d7bbb70cf20d0d17e2
Billy Peake <b.peake@illuscio.com>
2020-08-14 17:04:03 -0700

    updated runtime for heroku

--- stalkbroker/__main__.py
+++ stalkbroker/__main__.py
@@ -1,6 +1,9 @@
 import dotenv
+import sys
+import logging
 from stalkbroker import bot
 
 if __name__ == "__main__":
+    logging.info(f"Python Version: {sys.version}")
     dotenv.load_dotenv()
     bot.run_stalkbroker()
